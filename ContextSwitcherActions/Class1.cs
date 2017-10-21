91fb73456c95da4342422a2373135f1f3b80f460
mrsWolowitz <mrsWolowitz@users.noreply.github.com>
2017-10-21 17:51:06 +0300

    ReplaceWebConfig added

--- ContextSwitcherActions/Class1.cs
+++ ContextSwitcherActions/Class1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ContextSwitcherActions
 {
@@ -11,6 +13,8 @@ namespace ContextSwitcherActions
         public string DBPath { get; set; }
         public string Issue { get; set; }
 
+        private string DBName { get; set; }
+
         public Class1(string dbPath, string issue)
         {
             DBPath = dbPath;
@@ -18,7 +22,9 @@ namespace ContextSwitcherActions
         }
 
         public void RestoreDB()
-        { }
+        {
+            //string connectionString = 
+        }
 
         public void GetVersion()
         { }
@@ -27,7 +33,33 @@ namespace ContextSwitcherActions
         { }
 
         public void ReplaceWebConfig()
-        { }
+        {
+            string WebConfigPath = "WebSites\\Pure\\Site\\Web.config_";
+            string MyWebConfigPath = "WebSites\\Pure\\Site\\Web.config";
+
+            File.Copy(WebConfigPath, MyWebConfigPath, true);
+
+            XDocument config = XDocument.Load(MyWebConfigPath);
+            XElement configuration = config.Element("configuration");
+            XElement connectionStrings = configuration.Element("connectionStrings");
+
+            connectionStrings.RemoveNodes();
+            XElement projectx = new XElement("add",
+                new XAttribute("name", "ProjectX"),
+                new XAttribute("providerName", "System.Data.SqlClietnt"),
+                new XAttribute("connectionStrings", $"Data Source= msk-sql-1; Initial Catalog ={DBName}; Integrated Security = true;;"));
+            connectionStrings.Add(projectx);
+
+            XElement appSettings = config.Element("appSettings");
+
+            XElement customizationTempFilesPath = new XElement("add",
+                new XAttribute("key", "CustomizationTempFilesPath"),
+                new XAttribute("value", "C:\\Temp"));
+            appSettings.Add(customizationTempFilesPath);
+
+			config.Save(MyWebConfigPath);
+          
+        }
 
         public void ReplaceConnection()
         { }
